c4bb1d77583f15ff66aa7665e9a71c7e6abe5c7e
VishalInfoCoder <143776321+VishalInfoCoder@users.noreply.github.com>
2024-06-07 19:00:21 +0530

    crazy

--- app.py
+++ app.py
@@ -5,39 +5,14 @@ from sqlalchemy.exc import OperationalError
 from sqlalchemy import text
 import threading
 import time
-
+from flask_cors import CORS
 app = Flask(__name__)
 app.config.from_object(dbconnect.Config)
 db.init_app(app)
 
 from api.user_api_routes import user_route
 app.register_blueprint(user_route)
+CORS(app, origins='*')
 
-def check_db_connection():
-    try:
-        with app.app_context():
-            db.session.execute(text('SELECT 1'))
-            db.session.commit()
-        # print("Database connection is active.")
-    except OperationalError as e:
-        # print(f"Database connection lost. Reconnecting... Error: {e}")
-        with app.app_context():
-            db.session.rollback()
-            db.session.expire_all()
-            db.session.execute(text('SELECT 1'))
-            db.session.commit()
-        print("Database reconnected successfully.")
-    # except Exception as e:
-    #     print(f"An error occurred: {e}")
 
-def check_db_thread():
-    while True:
-        check_db_connection()
-        time.sleep(65)
 
-if __name__ == '__main__':
-    db_thread = threading.Thread(target=check_db_thread)
-    db_thread.daemon = True
-    db_thread.start()
-
-    app.run(debug=True, port=8080)
